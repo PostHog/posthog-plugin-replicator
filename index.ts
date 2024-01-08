db55c962baf74a94a4df05b8e8e7550a52e996b5
Dave Murphy <dave@posthog.com>
2024-01-08 14:36:31 -0800

    Update tests

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { Plugin } from '@posthog/plugin-scaffold'
+import { Plugin, Webhook } from '@posthog/plugin-scaffold'
 
-interface ReplicatorMetaInput {
+export interface ReplicatorMetaInput {
     config: {
         host: string
         project_api_key: string
@@ -10,44 +10,46 @@ interface ReplicatorMetaInput {
     }
 }
 
-type ReplicatorPlugin = Plugin<ReplicatorMetaInput>
+const plugin: Plugin<ReplicatorMetaInput> = {
+    composeWebhook: (event, { config }) => {
+        const replication = parseInt(config.replication) || 1
+        if (replication > 1) {
+            // This is a quick fix to make sure we don't become a spam bot
+            throw new Error('Replication factor > 1 is not allowed')
+        }
 
-export const composeWebhook: ReplicatorPlugin['composeWebhook'] = (event, { config }) => {
-    const replication = parseInt(config.replication) || 1
-    if (replication > 1) {
-        // This is a quick fix to make sure we don't become a spam bot
-        throw Error('Replication factor > 1 is not allowed')
-    }
+        const eventsToIgnore = new Set(
+            config.events_to_ignore && config.events_to_ignore.trim() !== ''
+                ? config.events_to_ignore.split(',').map((event) => event.trim())
+                : null
+        )
+        if (eventsToIgnore.has(event.event)) {
+            return null
+        }
 
-    const eventsToIgnore = new Set(
-        config.events_to_ignore && config.events_to_ignore.trim() !== ''
-            ? config.events_to_ignore.split(',').map((event) => event.trim())
-            : null
-    )
-    if (eventsToIgnore.has(event.event)) {
-        return null
-    }
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { team_id, ...sendableEvent } = { ...event, token: config.project_api_key }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { team_id, ...sendableEvent } = { ...event, token: config.project_api_key }
+        if (config.disable_geoip === 'Yes') {
+            sendableEvent.properties.$geoip_disable = true
+        }
 
-    if (config.disable_geoip === 'Yes') {
-        sendableEvent.properties.$geoip_disable = true
-    }
+        const batch = []
+        for (let i = 0; i < replication; i++) {
+            batch.push(sendableEvent)
+        }
 
-    const batch = []
-    for (let i = 0; i < replication; i++) {
-        batch.push(sendableEvent)
-    }
+        if (batch.length === 0) {
+            return null
+        }
 
-    if (batch.length === 0) {
-        return null
-    }
-
-    return {
-        url: `https://${config.host.replace(/\/$/, '')}/e`,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(batch),
-        method: 'POST',
-    }
+        return {
+            url: `https://${config.host.replace(/\/$/, '')}/e`,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(batch),
+            method: 'POST',
+        } as Webhook
+    },
 }
+
+export default plugin
